2f61b5bfb7eff7ac8ab3f68c093ed574add37103
rohit21755 <rohit.k09022002@gmail.com>
2025-05-26 21:38:49 +0530

    deposit context added

--- programs/lending/src/instructions/deposit.rs
+++ programs/lending/src/instructions/deposit.rs
@@ -1,19 +1,47 @@
 use anchor_lang::prelude::*;
-use anchor_spl::token_interface::{Mint};
-
-use crate::state::Bank;
+use anchor_spl::associated_token::AssociatedToken;
+use anchor_spl::token_interface::{ self, Mint, TokenAccount, TokenInterface, TransferChecked };
+use crate::state::{Bank, User};
 #[derive(Accounts)]
 pub struct Deposit<'info> {
     #[account(mut)]
     pub signer: Signer<'info>,
     pub mint: InterfaceAccount<'info, Mint>,
     #[account(
-        mut,
+        mut, 
         seeds = [mint.key().as_ref()],
         bump,
-    )]
+    )]  
     pub bank: Account<'info, Bank>,
+    #[account(
+        mut, 
+        seeds = [b"treasury", mint.key().as_ref()],
+        bump, 
+    )]  
+    pub bank_token_account: InterfaceAccount<'info, TokenAccount>,
+    #[account(
+        mut, 
+        seeds = [signer.key().as_ref()],
+        bump,
+    )]  
+    pub user_account: Account<'info, User>,
 
-    #[]
-
+    #[account( 
+        mut,
+        associated_token::mint = mint, 
+        associated_token::authority = signer,
+        associated_token::token_program = token_program,
+    )]
+    pub user_token_account: InterfaceAccount<'info, TokenAccount>, 
+    pub token_program: Interface<'info, TokenInterface>,
+    pub associated_token_program: Program<'info, AssociatedToken>,
+    pub system_program: Program<'info, System>,
 }
+
+// 1. CPI transfer from user's token account to bank's token account
+// 2. Calculate new shares to be added to the bank
+// 3. Update user's deposited amount and total collateral value
+// 4. Update bank's total deposited and total deposit shares
+// 5. Update users mental health factor ??
+
+pub fn 
